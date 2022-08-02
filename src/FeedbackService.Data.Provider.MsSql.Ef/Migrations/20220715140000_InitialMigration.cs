da97b22359c1c14f0d6ba849e050d5721b71dfbe
SolovyovDmitry <101804724+SolovyovDmitry@users.noreply.github.com>
2022-08-02 15:51:37 +0300

    Feature/create feedback service (#1)

---
+++ src/FeedbackService.Data.Provider.MsSql.Ef/Migrations/20220715140000_InitialMigration.cs
@@ -0,0 +1,20 @@
+﻿using LT.DigitalOffice.FeedbackService.Data.Provider.MsSql.Ef;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LT.DigitalOffice.OfficeService.Data.Migrations
+{
+  [DbContext(typeof(FeedbackServiceDbContext))]
+  [Migration("20220802120000_InitialMigration")]
+  public class InitialCreate : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+  }
+}
---
+++ src/FeedbackService.Data.Provider/IDataProvider.cs
@@ -0,0 +1,11 @@
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using LT.DigitalOffice.Kernel.EFSupport.Provider;
+using LT.DigitalOffice.Kernel.Enums;
+
+namespace LT.DigitalOffice.FeedbackService.Data.Provider
+{
+  [AutoInject(InjectType.Scoped)]
+  public interface IDataProvider : IBaseDataProvider
+  {
+  }
+}
