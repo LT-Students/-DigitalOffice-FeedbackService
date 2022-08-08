daeff7bc213207cecb79e7122864f8246c2048d6
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-08-08 15:20:23 +0300

    configure image service

--- src/FeedbackService.Models.Dto/Configurations/RabbitMqConfig.cs
+++ src/FeedbackService.Models.Dto/Configurations/RabbitMqConfig.cs
@@ -1,8 +1,12 @@
-﻿using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+﻿using LT.DigitalOffice.Kernel.BrokerSupport.Attributes;
+using LT.DigitalOffice.Kernel.BrokerSupport.Configurations;
+using LT.DigitalOffice.Models.Broker.Requests.Image;
 
 namespace LT.DigitalOffice.FeedbackService.Models.Dto.Configurations
 {
   public class RabbitMqConfig : BaseRabbitMqConfig
   {
+    [AutoInjectRequest(typeof(ICreateImagesRequest))]
+    public string CreateImagesEndpoint { get; set; }
   }
 }
