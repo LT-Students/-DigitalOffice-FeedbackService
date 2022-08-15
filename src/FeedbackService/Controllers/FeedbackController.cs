5e86c012c7551222f8f363822e0f3820361cc841
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-08-15 14:23:10 +0300

    add endpoint

--- src/FeedbackService/Controllers/FeedbackController.cs
+++ src/FeedbackService/Controllers/FeedbackController.cs
@@ -1,11 +1,11 @@
 ﻿using LT.DigitalOffice.FeedbackService.Business.Commands.Feedback.Interfaces;
+using LT.DigitalOffice.FeedbackService.Models.Dto.Models;
 using LT.DigitalOffice.FeedbackService.Models.Dto.Requests;
+using LT.DigitalOffice.FeedbackService.Models.Dto.Requests.Filter;
 using LT.DigitalOffice.Kernel.Responses;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Threading.Tasks;
-using LT.DigitalOffice.FeedbackService.Models.Dto.Models;
-using LT.DigitalOffice.FeedbackService.Models.Dto.Requests.Filter;
 
 namespace LT.DigitalOffice.FeedbackService.Controllers
 {
@@ -13,6 +13,14 @@ namespace LT.DigitalOffice.FeedbackService.Controllers
   [ApiController]
   public class FeedbackController : ControllerBase
   {
+    [HttpGet("get")]
+    public async Task<OperationResultResponse<FeedbackInfo>> GetAsync(
+      [FromQuery] Guid feedbackId,
+      [FromServices] IGetFeedbackCommand command)
+    {
+      return await command.ExecuteAsync(feedbackId);
+    }
+
     [HttpGet("find")]
     public async Task<FindResultResponse<FeedbackInfo>> FindAsync(
       [FromQuery] FindFeedbacksFilter filter,
