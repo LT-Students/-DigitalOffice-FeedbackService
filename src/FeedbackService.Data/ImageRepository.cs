37afe77628009b87c34487023597ea077df101ff
Dmitry Solovyov <dmitry.solovyov@lanit.tercom.com>
2022-08-15 14:23:20 +0300

    update repos

---
+++ src/FeedbackService.Data/ImageRepository.cs
@@ -0,0 +1,32 @@
+﻿using LT.DigitalOffice.FeedbackService.Data.Interfaces;
+using LT.DigitalOffice.FeedbackService.Data.Provider;
+using LT.DigitalOffice.FeedbackService.Models.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LT.DigitalOffice.FeedbackService.Data
+{
+  public class ImageRepository : IImageRepository
+  {
+    private readonly IDataProvider _provider;
+
+    public ImageRepository(IDataProvider provider)
+    {
+      _provider = provider;
+    }
+
+    public async Task<List<Guid>> CreateAsync(List<DbImage> dbImages)
+    {
+      if (dbImages is null || !dbImages.Any())
+      {
+        return null;
+      }
+
+      await _provider.Images.AddRangeAsync(dbImages);
+
+      return dbImages.Select(i => i.Id).ToList();
+    }
+  }
+}
---
+++ src/FeedbackService.Data/Interfaces/IImageRepository.cs
@@ -0,0 +1,14 @@
+﻿using LT.DigitalOffice.Kernel.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using LT.DigitalOffice.FeedbackService.Models.Db;
+
+namespace LT.DigitalOffice.FeedbackService.Data.Interfaces
+{
+  [AutoInject]
+  public interface IImageRepository
+  {
+    Task<List<Guid>> CreateAsync(List<DbImage> dbImages);
+  }
+}
