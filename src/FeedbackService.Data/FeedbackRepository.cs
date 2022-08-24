9e39da73a898257d5b535ebcee2e4698fc4666d7
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-08-24 20:07:05 +0300

    update repository

--- src/FeedbackService.Data/FeedbackRepository.cs
+++ src/FeedbackService.Data/FeedbackRepository.cs
@@ -1,6 +1,7 @@
 ﻿using LT.DigitalOffice.FeedbackService.Data.Interfaces;
 using LT.DigitalOffice.FeedbackService.Data.Provider;
 using LT.DigitalOffice.FeedbackService.Models.Db;
+using LT.DigitalOffice.FeedbackService.Models.Dto.Enums;
 using LT.DigitalOffice.FeedbackService.Models.Dto.Requests.Filter;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -51,14 +52,20 @@ namespace LT.DigitalOffice.FeedbackService.Data
 
       int totalCount = await query.CountAsync();
 
-      var dbFeedbacks = await (
-        from f
-        in query.Include(f => f.Images).Skip(filter.SkipCount).Take(filter.TakeCount).OrderByDescending(f => f.CreatedAtUtc)
-        select new { Feedback = f, ImagesCount = f.Images.Count }).ToListAsync();
+      var dbfeedbacks = query
+        .Skip(filter.SkipCount)
+        .Take(filter.TakeCount)
+        .Select(f => new { Feedback = f, ImagesCount = f.Images.Count })
+        .AsEnumerable()
+        .Select(f => (f.Feedback, f.ImagesCount));
 
-      return
-        (dbFeedbacks.Select(f => (f.Feedback, f.ImagesCount)).ToList(),
-        totalCount);
+      // TODO: Decide if it is better to sort before or after taking and skipping???
+      if (filter.OrderByDescending)
+      {
+        dbfeedbacks = dbfeedbacks.OrderByDescending(f => f.Feedback.CreatedAtUtc);
+      }
+
+      return (dbfeedbacks.ToList(), totalCount);
     }
 
     public Task<DbFeedback> GetAsync(Guid feedbackId)
@@ -77,6 +84,20 @@ namespace LT.DigitalOffice.FeedbackService.Data
       await _provider.SaveAsync();
 
       return dbFeedback.Id;
+    }
+
+    public async Task<bool> EditStatusesAsync(List<Guid> feedbackIds, FeedbackStatusType status)
+    {
+      var dbFeedbacks = _provider.Feedbacks.Where(f => feedbackIds.Contains(f.Id));
+      await dbFeedbacks.ForEachAsync(f => f.Status = (int)status);
+      await _provider.SaveAsync();
+
+      return true;
+    }
+
+    public Task<bool> HaveSameStatusAsync(List<Guid> feedbackIds, FeedbackStatusType status)
+    {
+      return _provider.Feedbacks.AnyAsync(f => feedbackIds.Contains(f.Id) && f.Status == (int)status);
     }
   }
 }
