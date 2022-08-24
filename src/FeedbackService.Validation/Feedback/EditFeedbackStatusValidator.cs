2b7f34dc84454e21a2b9b2d9bbe810a5425fa674
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-08-24 20:05:56 +0300

    add validator

---
+++ src/FeedbackService.Validation/Feedback/EditFeedbackStatusValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using LT.DigitalOffice.FeedbackService.Data.Interfaces;
+using LT.DigitalOffice.FeedbackService.Models.Dto.Requests;
+using LT.DigitalOffice.FeedbackService.Validation.Feedback.Interfaces;
+
+namespace LT.DigitalOffice.FeedbackService.Validation.Feedback
+{
+  public class EditFeedbackStatusValidator : AbstractValidator<EditFeedbackStatusesRequest>, IEditFeedbackStatusValidator
+  {
+    public EditFeedbackStatusValidator(IFeedbackRepository repository)
+    {
+      CascadeMode = CascadeMode.Stop;
+
+      RuleFor(f => f.FeedbackIds)
+        .Must(f => f.Count > 0)
+        .WithMessage("Feedback ids must be specified.");
+
+      RuleFor(f => f.Status)
+        .IsInEnum()
+        .WithMessage("Incorrect feedback status.");
+
+      RuleFor(r => r)
+        .MustAsync(async (r, _) => !await repository.HaveSameStatusAsync(r.FeedbackIds, r.Status))
+        .WithMessage("Some of feedbacks already has specified status.");
+    }
+  }
+}
