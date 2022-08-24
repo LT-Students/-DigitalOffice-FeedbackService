8d8b2052008b0a8d4980ec9a3d36f095fd055bea
DmitrySolovyov <dmitry.solovyov@lanit-tercom.com>
2022-08-24 20:05:41 +0300

    add request

---
+++ src/FeedbackService.Models.Dto/Requests/EditFeedbackStatusesRequest.cs
@@ -0,0 +1,15 @@
+﻿using LT.DigitalOffice.FeedbackService.Models.Dto.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LT.DigitalOffice.FeedbackService.Models.Dto.Requests
+{
+  public record EditFeedbackStatusesRequest
+  {
+    [Required]
+    public List<Guid> FeedbackIds;
+
+    [Required] public FeedbackStatusType Status;
+  }
+}
